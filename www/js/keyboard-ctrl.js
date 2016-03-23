7be35f42fec9ae4a6be3d9abeb9870963936a73d
Lucio Paoletti <lucio.paoletti@it.abb.com>
2016-03-23 17:39:56 +0100

    Fixed alternate keyboard keys with CAPS ON active, added CSS styles for holding keys

--- www/js/keyboard-ctrl.js
+++ www/js/keyboard-ctrl.js
@@ -11,40 +11,71 @@
       $event.stopImmediatePropagation();
     };
 
-    var capsLock = false;
-    var alt = false;
+    $scope.capsLock = false;
+    $scope.alt = false;
 
-    var keylayout = { a: ['\u00e1'],e: ['\u00e9'],i: ['\u00ed'],o: ['\u00f3'],u: ['\u00d3'],y: ['\u00fb'],
-      p: ['\u00f6'],c: ['\u00a9'],z:['\u00e6'],q:['\u00e4'],m:['\u00b5'],s:['\u00df'],',':['\u00e7'],'[':['\u00ab'],']':['\u00bb'],
-      9:['\u02bb'],0:['\u02bc'],1:['\u0021'],2:['\u00B2'],3:['\u00B3'],5:['\u20ac'],r:['\u00ae'],x:['\u0026'],'/':['\u003f']};
+    var keylayout = {
+      a: ['\u00e1'],
+      e: ['\u00e9'],
+      i: ['\u00ed'],
+      o: ['\u00f3'],
+      u: ['\u00fa'],
+      y: ['\u00fb'],
+      p: ['\u00f6'],
+      c: ['\u00a9'],
+      z: ['\u00e6'],
+      q: ['\u00e4'],
+      m: ['\u00b5'],
+      ',': ['\u00e7'],
+      '[': ['\u00ab'],
+      ']': ['\u00bb'],
+      9: ['\u02bb'],
+      0: ['\u02bc'],
+      1: ['\u0021'],
+      2: ['\u00B2'],
+      3: ['\u00B3'],
+      5: ['\u20ac'],
+      r: ['\u00ae'],
+      x: ['\u0026'],
+      '=': ['\u0040'],
+      '/': ['\u003f']
+    };
 
+    $scope.longKey = ['bksp', 'tab', 'shift', 'enter', 'clear', 'caps', 'alt'];
+    $scope.caps = ['caps'];
+    $scope.altKeys = [];
+    for (key in keylayout) {
+      $scope.altKeys.push(keylayout[key].toString());
+    }
 
-    $scope.keysConf = ['´ 1 2 3 4 5 6 7 8 9 0 - = bksp', 'tab q w e r t y u i o p [ ]', 'caps a s f g h j k l ; " enter', 'alt z x c v b n m , . /', 'space clear'];
+    $scope.keysConf = ['´ 1 2 3 4 5 6 7 8 9 0 - = BKSP', 'TAB q w e r t y u i o p [ ]', 'CAPS a s f g h j k l ; " ENTER', 'ALT z x c v b n m , . /', 'space clear'];
     var conf = $scope.keysConf;
     $scope.keyboardInput = '';
 
     function altKeyboard() {
-      alt = !alt;
-      //$scope.keysConf = ['´ 1 2 3 4 5 6 7 8 9 0 - = bksp', 'tab q w e r t y u i o p [ ]', 'caps a s f g h j k l ; " enter', 'alt z x c v b n m , . /', 'space clear'];
-      if(alt){
+      if ($scope.alt) {
         $scope.keysConf = $scope.keysConf.map(function (row) {
-          for(char in keylayout){
-            row = row.replace(' ' + char + ' ', ' ' + keylayout[char] + ' ')
+          for (char in keylayout) {
+            row = row.replace(' ' + ($scope.capsLock ? char.toUpperCase():char) + ' ', ' ' +  ($scope.capsLock ? keylayout[char].toString().toUpperCase():keylayout[char].toString())  + ' ');
           }
           return row;
         });
       }else{
-        $scope.keysConf = conf;
+        $scope.keysConf = $scope.keysConf.map(function (row) {
+          for (char in keylayout) {
+            row = row.replace(' ' + ($scope.capsLock ?  keylayout[char].toString().toUpperCase(): keylayout[char] )  + ' ', ' ' + ($scope.capsLock ? char.toUpperCase():char) + ' ');
+          }
+          return row;
+        });
       }
-
-
-      console.log( $scope.keysConf)
-
-
     }
 
-    function capslock() {
-      capsLock = !capsLock;
+
+    function capsLock() {
+      $scope.keysConf = $scope.keysConf.map(function (row) {
+        return $scope.capsLock  ? row.toUpperCase() : row.toLowerCase();
+      });
+        conf = $scope.keysConf;
     }
 
     function clearInput(input) {
@@ -76,13 +107,14 @@
       } else if (key.toLowerCase() == 'clear') {
         clearInput(input);
       } else if (key.toLowerCase() == 'alt') {
+        $scope.alt = !$scope.alt;
         altKeyboard();
       } else if (key.toLowerCase() == 'caps') {
-        capslock();
+        $scope.capsLock = !$scope.capsLock;
+        capsLock();
+      } else if (key.toLowerCase() == 'enter') {
+        $scope.showKey = false;
       } else {
-        if (capsLock) {
-          key = key.toUpperCase();
-        }
         applyNewChar(key, input);
       }
 
